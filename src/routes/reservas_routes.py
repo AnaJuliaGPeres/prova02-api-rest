1bc86fb3cbbd7c97e133f6f7ca6c043b04001556
Ana Julia G. Peres <126435035+AnaJuliaGPeres@users.noreply.github.com>
2023-11-27 21:36:47 -0300

    PrimeiroCommitProva

--- src/routes/reservas_routes.py
+++ src/routes/reservas_routes.py
@@ -3,6 +3,8 @@ import random
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from sqlmodel import select
+from fastapi import HTTPException
+
 
 from src.config.database import get_session
 from src.models.reservas_model import Reserva
@@ -29,23 +31,100 @@ def cria_reserva(reserva: Reserva):
                 content={"message": f"Voo com id {reserva.voo_id} não encontrado."},
                 status_code=404,
             )
+            
 
         # TODO - Validar se existe uma reserva para o mesmo documento
+    if session.exec(select(Reserva).where(Reserva.documento == reserva.documento)).first():
+                return JSONResponse(
+                    content={
+                    "message": f"Ja existe uma reserva para o documento {reserva.documento}."
+                },
+                status_code=400,
+            )
 
-        codigo_reserva = "".join(
+    codigo_reserva = "".join(
             [str(random.randint(0, 999)).zfill(3) for _ in range(2)]
         )
 
-        reserva.codigo_reserva = codigo_reserva
-        session.add(reserva)
-        session.commit()
-        session.refresh(reserva)
-        return reserva
+    reserva.codigo_reserva = codigo_reserva
+    session.add(reserva)
+    session.commit()
+    session.refresh(reserva)
+    return reserva
 
 
 @reservas_router.post("/{codigo_reserva}/checkin/{num_poltrona}")
 def faz_checkin(codigo_reserva: str, num_poltrona: int):
-    # TODO - Implementar reserva de poltrona
-    pass
+    with get_session() as session:
+        statement = select(Reserva).where(Reserva.codigo_reserva == codigo_reserva)
+        reserva = session.exec(statement).first()
 
-# TODO - Implementar troca de reserva de poltrona
+        if not reserva:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Reserva com o código {codigo_reserva} não encontrada."
+            )
+
+        statement = select(Voo).where(Voo.id == reserva.voo_id)
+        voo = session.exec(statement).first()
+
+        if not voo:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Voo com o ID {reserva.voo_id} não encontrado."
+            )
+
+        poltrona_atual = getattr(voo, f"poltrona_{num_poltrona}")
+
+        if poltrona_atual is not None:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Poltrona {num_poltrona} já ocupada."
+            )
+
+        setattr(voo, f"poltrona_{num_poltrona}", codigo_reserva)
+
+        session.add(voo)
+        session.commit()
+        session.refresh(voo)
+
+        return {"message": f"Check-in realizado com sucesso para a reserva {codigo_reserva} na poltrona {num_poltrona}."}
+
+
+        
+@reservas_router.patch("/{codigo_reserva}/checkin/{num_poltrona}")
+def faz_checkin_patch(codigo_reserva: str, num_poltrona: int):
+    with get_session() as session:
+        statement = select(Reserva).where(Reserva.codigo_reserva == codigo_reserva)
+        reserva = session.exec(statement).first()
+
+        if not reserva:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Reserva id {codigo_reserva} não foi encontrada."
+            )
+
+        statement = select(Voo).where(Voo.id == reserva.voo_id)
+        voo = session.exec(statement).first()
+
+        if not voo:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Voo : {reserva.voo_id} não foi encontrado."
+            )
+
+        poltrona_atual = getattr(voo, f"poltrona_{num_poltrona}")
+
+        if poltrona_atual is not None:
+            raise HTTPException(
+                status_code=403,
+                detail=f"A poltrona {num_poltrona} já foi reservada."
+            )
+
+        setattr(voo, f"poltrona_{num_poltrona}", codigo_reserva)
+
+        session.add(voo)
+        session.commit()
+        session.refresh(voo)
+
+        return {"message": f"Check-in realizado sobre a reserva com o codigo {codigo_reserva} sobre a poltrona {num_poltrona}."}
--- src/routes/voos_routes.py
+++ src/routes/voos_routes.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from sqlmodel import select
+from fastapi import HTTPException
+
 
 from src.config.database import get_session
 from src.models.voos_model import Voo
@@ -13,7 +15,7 @@ voos_router = APIRouter(prefix="/voos")
 @voos_router.post("")
 def cria_voo(voo: Voo):
     with get_session() as session:
-        LIMITE_HORAS = 5
+        LIMITE_HORAS = 2
         hora_atual = datetime.now()
         hora_limite = hora_atual + timedelta(hours=LIMITE_HORAS)
         no_horario_limite = voo.data_saida <= hora_limite
@@ -33,7 +35,7 @@ def cria_voo(voo: Voo):
 
 @voos_router.get("/vendas")
 def lista_voos_venda():
-    LIMITE_HORAS = 3
+    LIMITE_HORAS = 2
     with get_session() as session:
         hora_limite = datetime.now() + timedelta(hours=LIMITE_HORAS)
         statement = select(Voo).where(Voo.data_saida >= hora_limite)
@@ -47,5 +49,36 @@ def lista_voos():
         statement = select(Voo)
         voo = session.exec(statement).all()
         return voo
+    
+    # TODO - Implementar rota que retorne as poltronas por id do voo
+    
+@voos_router.get("/{voo_id}/poltronas")
+def obter_poltronas_por_voo(voo_id: int):
+    with get_session() as session:
+        # Verifica se o voo existe
+        voo = session.exec(select(Voo).where(Voo.id == voo_id)).first()
 
-# TODO - Implementar rota que retorne as poltronas por id do voo
+        if not voo:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Voo com ID {voo_id} não encontrado."
+            )
+
+        # Obtém as reservas associadas ao voo
+        reservas = session.exec(
+            select(Reserva).where(Reserva.voo_id == voo_id)
+        ).all()
+
+        poltronas_reservadas = [reserva.poltrona for reserva in reservas if reserva.poltrona is not None]
+
+        # Gera a lista de poltronas disponíveis (considerando um total fixo de poltronas no voo)
+        total_poltronas = voo.total_poltronas
+        poltronas_disponiveis = [i for i in range(1, total_poltronas + 1) if i not in poltronas_reservadas]
+
+        return {
+            "id_voo": voo.id,
+            "poltronas_reservadas": poltronas_reservadas,
+            "poltronas_disponiveis": poltronas_disponiveis
+        }
+
+
